9944fd308218bda491898289ff04aad4c865bf8b
Grégoire Ciles <bonjour@gregoireciles.fr>
2026-05-05 03:17:36 +0200

    feat(day_03): add part 1

---
+++ 2025/day_03/main_test.exs
@@ -0,0 +1,20 @@
+ExUnit.start()
+
+Code.require_file("main.exs", __DIR__)
+
+defmodule MainTest do
+  use ExUnit.Case
+
+  @example "987654321111111
+811111111111119
+234234234234278
+818181911112111"
+
+  test "part1" do
+    assert Main.part1(@example) == 357
+  end
+
+  test "part2" do
+    assert Main.part2(@example) == 0
+  end
+end
